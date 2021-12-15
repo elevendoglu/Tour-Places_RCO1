dd284b71c3e717b0cedcf8e7e4ae20d56f715a1b
Ertugrul Levendoglu <11doglu@gmail.com>
2021-12-15 20:07:03 +0200

    header added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
 
 export default App;
+
+
+// create own file (Navbar, Header ..) then recall them in div. 
+// recall css from .css file to .js file - 
+// recall .js file from .js file to app.js
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -14,3 +14,5 @@ const Navbar = () => {
 }
 
 export default Navbar;
+
+// import css style from external css file
