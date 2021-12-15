d55ae55369c493712e4bfbf7f089c33bbcc6f173
Ertugrul Levendoglu <11doglu@gmail.com>
2021-12-15 19:20:20 +0200

    navbar html added

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <a href="about us">ABOUT US</a>
+            <a href="for you">FOR YOU</a>
+            <a href="services">SERVICES</a>
+            <a href="blog">BLOG</a>
+            <a href="vlog">VLOG</a>
+            <a href="contact">CONTACT</a>        
+        </div>
+    )
+}
+
+export default Navbar;
