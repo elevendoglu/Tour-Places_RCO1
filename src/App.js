ba1befb7b960c9f4804a5c6f7dd0e6a79d5c49ff
Ertugrul Levendoglu <11doglu@gmail.com>
2021-12-15 21:12:36 +0200

    objects replaced by recallind data from data.js by console.log in Cards.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import Cards from './components/cards/Cards';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Header />
+      <Cards />
     </div>
   );
 }
